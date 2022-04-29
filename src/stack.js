56c88195b80023b9ef73aac5ef1bb62f626ea868
AndreyEfimenko <andrey.efimenko.sp@gmail.com>
2022-04-29 17:14:58 +0300

    feat: add stack

--- src/stack.js
+++ src/stack.js
@@ -12,21 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ class Stack {
+	constructor () {
+		this.array = [];
+	}
+	push(element) {
+		return this.array.push(element);
+	}
+	pop() {
+		return this.array.pop()
+	}
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	peek() {
+		return this.array[this.array.length - 1];
+	}
 }
 
 module.exports = {
